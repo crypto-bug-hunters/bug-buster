b5aee37e0e93256c4626814d86f6a09ce2b67dde
Guilherme Dantas <guidanoli@proton.me>
2023-10-18 12:25:35 -0300

    Use Cartesi shade of cyan

--- frontend/src/providers/theme.tsx
+++ frontend/src/providers/theme.tsx
@@ -1,6 +1,10 @@
 import { MantineThemeOverride, createTheme } from "@mantine/core";
+import { generateColors } from '@mantine/colors-generator';
 
 export const theme: MantineThemeOverride = createTheme({
     fontFamily: "Open Sans, sans-serif",
-    primaryColor: "blue",
+    primaryColor: "cartesi-cyan",
+    colors: {
+        'cartesi-cyan': generateColors('#00f6ff'),
+    },
 });
