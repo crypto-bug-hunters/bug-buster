6ac315c401d46b21feba751fc1227a70b876956a
Eduardo Bart <edub4rt@gmail.com>
2023-10-18 14:28:52 -0300

    Run SQLite bounty in safe mode

--- tests/bounties/sqlite-bounty/start.sh
+++ tests/bounties/sqlite-bounty/start.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 rm -f /tmp/test.db
-./sqlite3 /tmp/test.db <$1
+# -safe was introduced only in recent versions
+if sqlite3 -safe </dev/null 2>/dev/null; then
+    ./sqlite3 -safe /tmp/test.db <$1
+else
+    ./sqlite3 /tmp/test.db <$1
+fi
 status=$?
 rm -f /tmp/test.db
 # Status is always 139 when program crashes with "Segmentation fault" (SIGSEGV)
