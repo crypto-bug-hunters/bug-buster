261a5bc73853e1168453b7cc9e08666cf59d9c2b
Gabriel de Quadros Ligneul <8294320+gligneul@users.noreply.github.com>
2023-10-19 08:39:54 -0300

    Improve graphql query in frontend

--- frontend/src/model/reader.ts
+++ frontend/src/model/reader.ts
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import { useQuery } from "@apollo/client";
 import { gql } from "./__generated__/gql";
 import { CompletionStatus } from "./__generated__/graphql";
@@ -36,24 +37,6 @@ const GET_LAST_INPUTS = gql(/* GraphQL */ `
   }
 `);
 
-function getLastAcceptedInputIndex(): ReaderResult<number | undefined> {
-  const { loading, error, data } = useQuery(GET_LAST_INPUTS, {
-    variables: {},
-  });
-  if (loading) return { state: "loading" };
-  if (error) return { state: "error", message: error.message };
-
-  console.log(data);
-  if (data?.inputs?.totalCount === 0) {
-    return { state: "success", response: undefined };
-  }
-  let edge = data?.inputs?.edges.findLast(
-    (e) => e.node.status === CompletionStatus.Accepted,
-  );
-  let index = edge?.node.index;
-  return { state: "success", response: index };
-}
-
 const GET_INPUT = gql(/* GraphQL */ `
   query getInput($inputIndex: Int!) {
     input(index: $inputIndex) {
@@ -83,32 +66,41 @@ const GET_INPUT = gql(/* GraphQL */ `
   }
 `);
 
-function GetLatestState(): ReaderResult<BugLessState> {
-  let result = getLastAcceptedInputIndex();
-
-  // We should only return after useQuery to avoid the "Rendered more hooks
-  // than during the previous render" error. Hence, we initialize the input
-  // index here.
-  let inputIndex = 0;
-  if (result.state == "success") {
-    if (result.response !== undefined) {
-      inputIndex = result.response;
-    }
+// Get the latest bug less state from the GraphQL API polling the API every 500 ms.
+function GetLatestState(): ReaderResult<BugLessState | null> {
+  const lastInputsQuery = useQuery(GET_LAST_INPUTS, {
+    pollInterval: 500, // ms
+  });
+  let edge = lastInputsQuery.data?.inputs?.edges.findLast(
+    (e) => e.node.status === CompletionStatus.Accepted
+  );
+  let inputIndex = edge?.node.index;
+  const inputQuery = useQuery(GET_INPUT, {
+    skip: !inputIndex,
+    variables: {
+      inputIndex: inputIndex as number, // this cast is fine because of skip above
+    },
+  });
+  if (lastInputsQuery.loading) return { state: "loading" };
+  if (lastInputsQuery.error) {
+    return { state: "error", message: lastInputsQuery.error.message };
+  }
+  if (inputQuery.loading) return { state: "loading" };
+  if (inputQuery.error) {
+    return { state: "error", message: inputQuery.error.message };
   }
 
-  const { loading, error, data } = useQuery(GET_INPUT, {
-    variables: { inputIndex },
-  });
-  if (result.state != "success") return result;
-  if (loading) return { state: "loading" };
-  if (error) return { state: "error", message: error.message };
+  if (inputIndex === null) {
+    return { state: "success", response: null };
+  }
 
-  let reportEdge = data?.input.reports.edges.find((edge) =>
-    edge.node.payload.startsWith("0x01"),
+  let reportEdge = inputQuery.data?.input.reports.edges.find((edge) =>
+    edge.node.payload.startsWith("0x01")
   );
+
   if (reportEdge === undefined) {
     // This should never happen because the input was accepted.
-    return { state: "error", message: "missing state from accepted input" };
+    return { state: "error", message: "missing return from accepted input" };
   }
 
   // Check codec encoding.
@@ -122,7 +114,6 @@ function GetLatestState(): ReaderResult<BugLessState> {
   }
   let stateText = new TextDecoder().decode(stateBytes);
   let stateJson = JSON.parse(stateText) as BugLessState;
-
   return { state: "success", response: stateJson };
 }
 
