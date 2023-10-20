2422a0189df0f7a42ceaf754109a1657a9d1cf4f
Francisco Moura <franciscoeduardomoura@gmail.com>
2023-10-19 23:31:36 -0300

    Add Sponsorship and Exploit details to Bounty Page

--- frontend/src/app/bounty/[bounty_id]/page.tsx
+++ frontend/src/app/bounty/[bounty_id]/page.tsx
@@ -10,12 +10,37 @@ import {
     Image,
     Title,
     useMantineTheme,
+    Card,
+    Text,
 } from "@mantine/core";
 
 import { Address, bytesToHex, toHex, Hex } from "viem";
 
 import { GetBounty } from "../../../model/reader";
 import Link from "next/link";
+import { Sponsorship } from "../../../model/state";
+
+const Sponsor: FC<{ index: number; sponsorship: Sponsorship }> = ({
+    index,
+    sponsorship,
+}) => {
+    return (
+        <Card>
+            <Stack justify="center" align="center">
+                <Card.Section>
+                    <Image h={300} src={sponsorship.Sponsor.ImgLink} />
+                </Card.Section>
+
+                <Text fw={500} size="lg" mt="md">
+                    {sponsorship.Sponsor.Name}
+                </Text>
+                <Text size="sm" c="dimmend">
+                    {sponsorship.Value}
+                </Text>
+            </Stack>
+        </Card>
+    );
+};
 
 const BountyInfoPage: FC<{ params: { bounty_id: number } }> = ({
     params: { bounty_id },
@@ -33,6 +58,7 @@ const BountyInfoPage: FC<{ params: { bounty_id: number } }> = ({
         case "success":
             const bounty = result.response;
             const profile = bounty.Developer;
+            const hasExploit = !!bounty.Exploit;
             let totalPrize = 0;
             const sponsors = bounty.Sponsorships?.forEach((sponsorship) => {
                 totalPrize += parseInt(sponsorship.Value);
@@ -44,21 +70,57 @@ const BountyInfoPage: FC<{ params: { bounty_id: number } }> = ({
                             <Title order={2}>{profile.Name}</Title>
                             <Image w={300} src={bounty.Developer.ImgLink} />
                             {bounty.Description}
-                            <Title order={3}>
-                                Total Prize: {totalPrize} wei
-                            </Title>
-                            <Group justify="left">
-                                <Link
-                                    href={"/bounty/" + bounty_id + "/sponsor"}
-                                >
-                                    <Button>Add Sponsorship</Button>
-                                </Link>
-                                <Link
-                                    href={"/bounty/" + bounty_id + "/exploit"}
-                                >
-                                    <Button>Send Exploit</Button>
-                                </Link>
-                            </Group>
+                            {!hasExploit && (
+                                <>
+                                    <Title order={3}>
+                                        Total Prize: {totalPrize} wei
+                                    </Title>
+                                    <Group justify="left">
+                                        <Link
+                                            href={
+                                                "/bounty/" +
+                                                bounty_id +
+                                                "/sponsor"
+                                            }
+                                        >
+                                            <Button>Add Sponsorship</Button>
+                                        </Link>
+                                        <Link
+                                            href={
+                                                "/bounty/" +
+                                                bounty_id +
+                                                "/exploit"
+                                            }
+                                        >
+                                            <Button>Send Exploit</Button>
+                                        </Link>
+                                    </Group>
+                                </>
+                            )}
+                            {hasExploit && (
+                                <>
+                                    <Title order={2}>Exploited by </Title>
+                                    <Title order={1}>
+                                        {bounty.Exploit?.Hacker.Name}
+                                    </Title>
+                                    <Title order={3}>
+                                        {bounty.Exploit?.Hacker.Address}
+                                    </Title>
+                                    <Image
+                                        w={300}
+                                        src={bounty.Exploit?.Hacker.ImgLink}
+                                    />
+                                </>
+                            )}
+                            <Title order={2}>Sponsors</Title>
+                            {bounty.Sponsorships?.map((sponsorship, index) => {
+                                return (
+                                    <Sponsor
+                                        index={index}
+                                        sponsorship={sponsorship}
+                                    />
+                                );
+                            })}
                         </Stack>
                     </Box>
                 </Center>
