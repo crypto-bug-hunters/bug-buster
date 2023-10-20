f4ece9654e09f2b60ca8352f59a91cc12279e832
Guilherme Dantas <guidanoli@proton.me>
2023-10-20 03:50:32 -0300

    Add components directory

---
+++ frontend/src/components/copy.tsx
@@ -0,0 +1,52 @@
+import { FC } from "react";
+
+import {
+    CopyButton,
+    Tooltip,
+    ActionIcon,
+    rem,
+    Code,
+    Group,
+    Text,
+} from "@mantine/core";
+
+import { IconCopy, IconCheck } from '@tabler/icons-react';
+
+export const CodeWithCopyButton: FC<{ value: string }> = ({ value }) => {
+    return (
+        <Group>
+            <Code>{value}</Code>
+            <CopyButtonWithIcon value={value} />
+         </Group>
+    );
+};
+
+export const CopyButtonWithIcon: FC<{ value: string }> = ({ value }) => {
+    return (
+       <CopyButton value={value} timeout={2000}>
+            {({ copied, copy }) => (
+                <Tooltip
+                    label={copied ? "Copied" : "Copy"}
+                    withArrow
+                    position="right"
+                >
+                    <ActionIcon
+                        color={copied ? "teal" : "gray"}
+                        variant="subtle"
+                        onClick={copy}
+                    >
+                        {copied ? (
+                            <IconCheck
+                                style={{ width: rem(16) }}
+                            />
+                        ) : (
+                            <IconCopy
+                                style={{ width: rem(16) }}
+                            />
+                        )}
+                    </ActionIcon>
+                </Tooltip>
+            )}
+        </CopyButton>
+    );
+};
