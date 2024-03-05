36c5aa03742ca4807eb7460739e5ea16482dcd05
Enderson Maia <endersonmaia@gmail.com>
2024-03-05 11:41:11 +0000

    feat: accept GRAPHQL_ENDPOINT from environment during build

--- frontend/src/providers/graphqlProvider.tsx
+++ frontend/src/providers/graphqlProvider.tsx
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 const client = new ApolloClient({
-    uri: "http://localhost:8080/graphql",
+    uri: process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT || "http://localhost:8080/graphql",
     cache: new InMemoryCache(),
 });
 
