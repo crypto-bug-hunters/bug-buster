57ef7700310a1afdb1ca88fabef4448967fa61ef
Cláudio Silva <claudio.engdist@gmail.com>
2024-03-17 20:23:15 -0300

    Adds support for optimism

--- frontend/src/providers/walletProvider.tsx
+++ frontend/src/providers/walletProvider.tsx
@@ -2,13 +2,21 @@
 import { createWeb3Modal, defaultWagmiConfig } from "@web3modal/wagmi/react";
 
 import { configureChains, WagmiConfig } from "wagmi";
-import { foundry, mainnet, sepolia } from "wagmi/chains";
+import {
+    foundry,
+    mainnet,
+    sepolia,
+    optimismSepolia,
+    optimism,
+} from "wagmi/chains";
 import { publicProvider } from "wagmi/providers/public";
 
 // select chain based on env var
 const chainId = parseInt(process.env.NEXT_PUBLIC_CHAIN_ID || "31337");
 const chain =
-    [foundry, mainnet, sepolia].find((c) => c.id == chainId) || foundry;
+    [foundry, mainnet, sepolia, optimismSepolia, optimism].find(
+        (c) => c.id == chainId,
+    ) || foundry;
 
 // only 1 chain is enabled, based on env var
 const { chains } = configureChains([chain], [publicProvider()]);
