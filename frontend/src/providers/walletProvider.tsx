3525ddbd764cd07ec272aa5e31c8eb89fbf72672
Cláudio Silva <claudio.engdist@gmail.com>
2024-03-18 10:31:28 -0300

    Refactor for better identation

--- frontend/src/providers/walletProvider.tsx
+++ frontend/src/providers/walletProvider.tsx
@@ -12,11 +12,9 @@ import {
 import { publicProvider } from "wagmi/providers/public";
 
 // select chain based on env var
-const chainId = parseInt(process.env.NEXT_PUBLIC_CHAIN_ID || "31337");
-const chain =
-    [foundry, mainnet, sepolia, optimismSepolia, optimism].find(
-        (c) => c.id == chainId,
-    ) || foundry;
+const supportedChains = [foundry, mainnet, sepolia, optimismSepolia, optimism];
+const selectedChainId = parseInt(process.env.NEXT_PUBLIC_CHAIN_ID || "31337");
+const chain = supportedChains.find((c) => c.id == selectedChainId) || foundry;
 
 // only 1 chain is enabled, based on env var
 const { chains } = configureChains([chain], [publicProvider()]);
