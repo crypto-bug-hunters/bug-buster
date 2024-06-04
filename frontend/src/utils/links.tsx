f1169164694237e25b05f3aa6d0974bc69b42094
Guilherme Dantas <guidanoli@proton.me>
2024-06-04 10:17:54 -0300

    Move external links to a single file

---
+++ frontend/src/utils/links.tsx
@@ -0,0 +1,6 @@
+export const GH_REPO_URL = "https://github.com/crypto-bug-hunters/bug-buster";
+export const GH_MAIN_BRANCH = "main";
+export const GH_README_URL = `${GH_REPO_URL}/blob/${GH_MAIN_BRANCH}/README.md`;
+export const DISCORD_CHANNEL_URL =
+    "https://discord.com/channels/600597137524391947/1166042819782258788";
+export const X_ACCOUNT_URL = "https://x.com/BugBusterApp";
