f6acc9324044cfbf49218fd889d92d4cf706ff4c
Cláudio Silva <claudio.engdist@gmail.com>
2024-08-06 08:28:40 -0300

    fix: check preconditions before running exploit code for the solidity bounty (#126)
    * fix: check preconditions before running exploit code
    
    * fix:  change to check only for expected return codes

--- tests/bounties/solidity-bounty/start.sh
+++ tests/bounties/solidity-bounty/start.sh
@@ -1,4 +1,31 @@
 #!/bin/sh
+
+#
+# Validate exploit code - Exploit code file must contain only ascii characters
+#
+grep -q "[^[:print:]]" $1 
+status=$?
+if [ $status -ne 1 ]; then
+    >&2 echo "Invalid exploit code: Error searching for non-ascii characters"
+    exit 1
+fi
+
+
+#
+# Validate exploit code - No experimental directives are allowed
+# See: https://github.com/ethereum/solidity/issues/15223
+#
+grep -q "experimental" $1
+status=$?
+if [ $status -ne 1 ]; then
+    >&2 echo "Invalid exploit code: Error searching for 'experimental' keyword"
+    exit 1
+fi
+
+
+#
+# Run the exploit code
+#
 ./solc $1
 status=$?
 # Status is always 139 when program crashes with "Segmentation fault" (SIGSEGV)
