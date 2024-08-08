f6e89395af5fc052fba59fb1128b4216aed1dccc
Cláudio Silva <claudio.engdist@gmail.com>
2024-08-08 14:54:36 -0300

    fix: Replace Discord thread by the new Telegram group (#127)

--- frontend/src/utils/links.tsx
+++ frontend/src/utils/links.tsx
@@ -1,8 +1,7 @@
 export const GH_REPO_URL = "https://github.com/crypto-bug-hunters/bug-buster";
 export const GH_MAIN_BRANCH = "main";
 export const GH_README_URL = `${GH_REPO_URL}/blob/${GH_MAIN_BRANCH}/README.md`;
-export const DISCORD_CHANNEL_URL =
-    "https://discord.com/channels/600597137524391947/1166042819782258788";
+export const TELEGRAM_CHANNEL_URL = "https://t.me/+G_CPMEhCHC04MzA5";
 export const X_ACCOUNT_URL = "https://x.com/BugBusterApp";
 export const GOOGLE_BOUNTY_CREATION_FORM_URL =
     "https://forms.gle/h4EePFXG41Zwv48a7";
