4624c86332ede63f0428065b10c42a7bd643eaaf
Cláudio Silva <claudio.engdist@gmail.com>
2024-08-09 00:38:42 -0300

    fix: Landing page responsive

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,40 +1,54 @@
 "use client";
 import { FC } from "react";
-import { Button, Stack, Card, Text, Group } from "@mantine/core";
+import { Button, Card, Text, Flex, Title } from "@mantine/core";
 import Link from "next/link";
 import { GH_README_URL } from "../utils/links";
 
 const Home: FC = () => {
     return (
-        <Stack align="center">
-            <Card mt={20} ml={20} mr={20} withBorder>
-                <Stack mt="md" mb="md" align="center">
-                    <Text size="40px">A Trustless</Text>
-                    <Text size="40px">Bug Bounty Platform</Text>
-                </Stack>
+        <Flex direction={"row"} justify={"center"}>
+            <Card
+                mt={20}
+                ml={20}
+                mr={20}
+                withBorder
+                w={{ base: "300px", sm: "720px" }}
+            >
+                <Flex mt="md" mb="md" direction={"column"} align={"center"}>
+                    <Title>A Trustless Bug Bounty Platform</Title>
 
-                <Text c="dimmed" w={720} mb="md">
-                    Bug Buster accepts software written in any major programming
-                    language. Through a friendly web interface, hackers can test
-                    their exploits right on the browser, without even having to
-                    sign Web3 transactions! Once the hacker finds a valid
-                    exploit, they can finally send a transaction requesting the
-                    reward to be transferred to their account. All major wallets
-                    are supported!
-                </Text>
+                    <Text c="dimmed" mb="md">
+                        Bug Buster accepts software written in any major
+                        programming language. Through a friendly web interface,
+                        hackers can test their exploits right on the browser,
+                        without even having to sign Web3 transactions! Once the
+                        hacker finds a valid exploit, they can finally send a
+                        transaction requesting the reward to be transferred to
+                        their account. All major wallets are supported!
+                    </Text>
+                </Flex>
 
-                <Group justify="space-between">
-                    <Link href={"/explore"}>
-                        <Button size="lg">Explore Bounties</Button>
-                    </Link>
-                    <Link href={GH_README_URL}>
-                        <Button variant="outline" size="lg">
+                <Flex
+                    direction={{ base: "column", sm: "row" }}
+                    gap="md"
+                    justify={"space-between"}
+                >
+                    <Link
+                        href={GH_README_URL}
+                        style={{ textDecoration: "none" }}
+                    >
+                        <Button fullWidth variant="outline" size="lg">
                             Learn more
                         </Button>
                     </Link>
-                </Group>
+                    <Link href={"/explore"} style={{ textDecoration: "none" }}>
+                        <Button fullWidth size="lg">
+                            Explore Bounties
+                        </Button>
+                    </Link>
+                </Flex>
             </Card>
-        </Stack>
+        </Flex>
     );
 };
 
