04887a12bad6d4ad1c3f416203e8ce85d38d03f4
Guilherme Dantas <guidanoli@proton.me>
2024-10-28 15:41:04 -0300

    Smart contract bounties (#169)
    * Add `forge-std` to the machine
    * Bump builtins from 0.6.0 to 0.7.2 and add `cast`
    * Allow syscalls and increase memory and time limits
    * Add smart contract bounty example

---
+++ tests/bounties/src/adder/src/unsafe/Adder.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: Apache-2.0
+
+pragma solidity ^0.8.27;
+
+import {IAdder} from "../IAdder.sol";
+
+contract UnsafeAdder is IAdder {
+    uint256 public number = 1;
+
+    function add(uint256 x) external override {
+        unchecked {
+            number += x;
+        }
+    }
+}
